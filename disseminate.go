fd8f4617cd53bb15f83158cf6a4bac3b6463ac83
Bob Rockers <bob@rockerssoft.org>
2018-06-22 17:09:23 -0500

    Added timestamp of actual run, and min/max checks.

--- disseminate.go
+++ disseminate.go
@@ -2,7 +2,7 @@ package main
 
 import "os"
 import "flag"
-// import "io/ioutil"
+import "time"
 import "os/exec"
 import "fmt"
 import "regexp"
@@ -14,14 +14,20 @@ type response struct {
 		Commit  string `json:"commit"`
 		Message string `json:"message"`
 		Date    string `json:"timestamp"`
+		Time    string `json:"buildtime"`
 }
 
 func main(){
 
+	// Message conditions
+	var maxLenth int = 280
+	var minLenth int = 30
+
 	// var number string
 	var message string
 	var hash []string
-	var time []string
+	var commitTime []string
+	now := time.Now()
 
 	// Regular Expression filters
 	var authTag = regexp.MustCompile(`Author: .*\n`)
@@ -30,11 +36,24 @@ func main(){
 	var mer1Tag = regexp.MustCompile(`Merge pull request .*\n`)
 	var mer2Tag = regexp.MustCompile(`Merge: .*\n`)
 
+	// Build our timestamp
+	timestamp := []string{
+		strconv.Itoa(now.Year()),
+		fmt.Sprintf("%02d", int(now.Month())),
+		strconv.Itoa(now.Day()),
+		strconv.Itoa(now.Hour()),
+		strconv.Itoa(now.Minute()) }
+
 	// Less simpler printing
 	p := fmt.Println
 	// wordPtr := flag.String("filter", "merge", "a string")
-	numbPtr := flag.Int("count", 1, "An integer identifying the number of previous commits to check.")
+	numbPtr := flag.Int("count", 1, "Number of previous commit messages to return.")
+	maxLenthPtr := flag.Int("max", maxLenth, "Maximum allowable length of the commit message.")
+	minLenthPtr := flag.Int("min", minLenth, "Minimum allowable length of the commit message.")
 	flag.Parse()
+
+	maxLenth = *maxLenthPtr
+	minLenth = *minLenthPtr
 
 	// exec.Command requires a single string for third arg.  Combine strings
 	s := []string{ "git log -n", strconv.Itoa(*numbPtr) }
@@ -57,7 +76,7 @@ func main(){
 	}
 
 	hash = commTag.FindStringSubmatch(message)
-	time = dateTag.FindStringSubmatch(message)
+	commitTime = dateTag.FindStringSubmatch(message)
 
 	// Store the message as a hash document
 	if hash[1] == "" {
@@ -66,7 +85,7 @@ func main(){
 	}
 
 	// Store the datestamp
-	if time[1] == "" {
+	if commitTime[1] == "" {
 		p(message)
 		panic("Was not able to get the date/time value for the git commit message. ")
 	}
@@ -86,15 +105,24 @@ func main(){
 		message = mer2Tag.ReplaceAllString(message, "")
 		message = strings.TrimSpace(message)
 
+		// Message tests, to make sure they have a certain "quality"
 		if message == "" {
 			p("ERROR: Commit message is empty.")
+			os.Exit(1)
+		} else if len(message) <= minLenth {
+			p("ERROR: Commit message does not meet minimum allowable length.")
+			os.Exit(1)
+		} else if len(message) >= maxLenth {
+			p("ERROR: Commit message exceeds the maximum allowable length.")
 			os.Exit(1)
 		} else  {
 			// p("SUCCESS:", hash[1])
 			// p(message)
+			// timestamp, _ := now.MarshalJSON()
 			res := &response{
 				Commit: hash[1],
-				Date: time[1],
+				Date: commitTime[1],
+				Time: strings.Join(timestamp, ""),
 				Message: message}
 			resJson, _ := json.Marshal(res)
 			p(string(resJson))
