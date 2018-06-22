2b63371d924086b37cb3428352d1441addca2d63
Bob Rockers <bob@rockerssoft.org>
2018-06-22 12:46:52 -0500

    Working with new filters and getting the hash value from the commit message.

--- disseminate.go
+++ disseminate.go
@@ -4,44 +4,32 @@ package main
 import "flag"
 // import "io/ioutil"
 import "os/exec"
-// import "strings"
 import "fmt"
 import "regexp"
 import "strings"
-// import "github.com/brockers/gitlogutil"
-// import "github.com/davecgh/go-spew/spew"
 import "strconv"
 
 func main(){
 
 	// var number string
+	var message string
+	var hash []string
 
+	// Regular Expression filters
 	var authTag = regexp.MustCompile(`Author: .*\n`)
 	var dateTag = regexp.MustCompile(`Date: .*\n`)
-	var commTag = regexp.MustCompile(`commit [a-z0-9]*\n`)
-	var message string
+	var commTag = regexp.MustCompile(`commit ([a-z0-9]*)\n`)
+	var mer1Tag = regexp.MustCompile(`Merge pull request .*\n`)
+	var mer2Tag = regexp.MustCompile(`Merge: .*\n`)
+
 	// wordPtr := flag.String("filter", "merge", "a string")
 	numbPtr := flag.Int("count", 1, "an int")
-
 	flag.Parse()
-	count := *numbPtr
 
-	// message := "Revese argument example"
-	cmdmsg := "bash"
-	s := []string{ "git log -n", strconv.Itoa(count), "--grep merge" }
-	gitmsg := strings.Join(s, "  ")
-	// number = "1"
+	// exec.Command requires a single string for third arg.  Combine strings
+	s := []string{ "git log -n", strconv.Itoa(*numbPtr) }
 
-	// fmt.Println("PATH: ", os.Getenv("PATH"))
-	// fmt.Println(message, gitlogutil.Reverse(message))
-	// number = strconv.Itoa(*numbPtr)
-	// fmt.Println("filter: ", *wordPtr)
-	// fmt.Println("count: ", number)
-	// fmt.Println("arguments: ", flag.Args())
-	// , gitlogutil.Reverse(strings.Join(argsWithProg) ))
-	// --merges
-	// if wordPtr != nil {
-	gitlogCmd := exec.Command(cmdmsg, "-c", gitmsg)
+	gitlogCmd := exec.Command("bash", "-c", strings.Join(s, "  "))
 	gitlogOut, err := gitlogCmd.CombinedOutput()
 
 	if err != nil {
@@ -49,14 +37,41 @@ func main(){
 		// fmt.Println("ERROR:", log)
 		fmt.Println("ERROR", gitlogOut)
 		panic(err)
-	} else {
-		message = string(gitlogOut)
 	}
 
-  message = authTag.ReplaceAllString(message, "")
-  message = dateTag.ReplaceAllString(message, "")
-  message = commTag.ReplaceAllString(message, "")
-  message = strings.TrimSpace(message)
+	message = string(gitlogOut)
+	// First test is if the most recent update actually has a merge message
+	if message == "" {
+		fmt.Println("No message was obtained from git log")
+	}
 
-	fmt.Println(message)
+	hash = commTag.FindStringSubmatch(message)
+	// Store the message as a hash document
+	if hash[1] == "" {
+		fmt.Println(message)
+		panic("Was not able to get the hash value for the git commit message. ")
+	}
+
+
+	is_merge := mer1Tag.MatchString(message)
+
+	if ! is_merge {
+		fmt.Println("Last commit was not a merge")
+	} else {
+
+		// Now we remove the author, date, and commit from the message
+		message = authTag.ReplaceAllString(message, "")
+		message = dateTag.ReplaceAllString(message, "")
+		message = commTag.ReplaceAllString(message, "")
+		message = mer1Tag.ReplaceAllString(message, "")
+		message = mer2Tag.ReplaceAllString(message, "")
+		message = strings.TrimSpace(message)
+
+		if message == "" {
+			panic("Commit message is empty.")
+		} else  {
+			fmt.Println("SUCCESS:", hash[1])
+			fmt.Println(message)
+		}
+	}
 }
