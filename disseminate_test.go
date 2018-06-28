a826881d37b3f98526d702db4324b4bb0d3da89e
Bob Rockers <bob@rockerssoft.org>
2018-06-28 11:12:17 -0500

    Checking out recover to test panic calls in go.

--- disseminate_test.go
+++ disseminate_test.go
@@ -2,6 +2,18 @@ package main
 
 import "testing"
 
+func Testwarn(t *testing.T){
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The warn function did not panic")
+		}
+	}()
+	// Check out warn and see if it runs
+	warn("Error message on panic.")
+	// Output:
+	// ERROR: Error message on panic.
+}
+
 func TestgetHashString(t *testing.T) {
 
 	cases := []struct{
@@ -17,4 +29,7 @@ func TestgetHashString(t *testing.T) {
 			t.Errorf("getHashString(%q) == %q, want %q", c.in, got, c.want)
 		}
 	}
+	// Output:
+	// a98d9e63902bad87b3d8
+	// XXX
 }
