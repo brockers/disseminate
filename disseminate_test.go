6c8a9a0032f10c76299ca071dba8f1efa7bee19b
Bob Rockers <bob@rockerssoft.org>
2018-07-03 17:02:05 -0500

    Stopping for today, this os.Exit(1) hack is really ugly.

--- disseminate_test.go
+++ disseminate_test.go
@@ -2,20 +2,8 @@ package main
 
 import "testing"
 import "errors"
-
-// func TestWarn(t *testing.T){
-// 	defer func() {
-// 		if r := recover(); r == nil {
-// 			fmt.Println("Roveredin f", r)
-// 			// t.Errorf("The warn function did not panic")
-// 		}
-// 	}()
-// 	// Check out warn and see if it runs
-// 	warn("Error message on panic.")
-// 	t.Errorf("Code id not panic")
-// 	// Output:
-// 	// ERROR: Error message on panic.
-// }
+import "os"
+import "os/exec"
 
 func TestCheck(t *testing.T){
 
@@ -32,8 +20,26 @@ func TestCheck(t *testing.T){
 	err = errors.New("Something bad happened")
 	// Check out warn and see if it runs
 	check(err, "Error message on panic.")
-	// Output:
-	// ERROR: Error message on panic.
+}
+
+func TestWarn(t *testing.T){
+	if os.Getenv("LOTS_OF_CRASHES") == "1" {
+		// Check out warn and see if it runs
+		warn("Error message on panic.")
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestWarn")
+	cmd.Env = append(os.Environ(), "LOTS_OF_CRASHES=1")
+	stdout, _ := cmd.StderrPipe()
+	if err := cmd.Start; err != nil {
+		t.Errorf("Code id not panic")
+	}
+
+  got := string(stdout)
+  expected := "ERROR: Error message on panic."
+  if !strings.HasSuffix(got[:len(got)-1], expected) {
+    t.Fatalf("Unexpected warn message. Got %s but should contain %s", got[:len(got)-1], expected)
+  }
 }
 
 // func TestGetHashString(t *testing.T) {
